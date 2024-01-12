fc66ab2e997099747dc60f88381a89bf8af19311
Rijk van Zanten <rijkvanzanten@me.com>
2024-01-12 18:04:23 -0500

    Render type buttons

--- app/src/modules/settings/routes/marketplace/registry.vue
+++ app/src/modules/settings/routes/marketplace/registry.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import SearchInput from '@/views/private/components/search-input.vue';
-import { ref, watchEffect, watch } from 'vue';
+import { ref, watchEffect, watch, computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import SettingsNavigation from '../../components/navigation.vue';
 import api from '@/api';
 import { debounce } from 'lodash';
+import { EXTENSION_TYPES } from '@directus/extensions';
 
 const { t } = useI18n();
 
@@ -17,25 +18,29 @@ watch(
 	}, 450),
 );
 
+const perPage = 6;
 const search = ref('');
-const page = ref(0);
-const perPage = ref(20);
+const page = ref(1);
 const type = ref<string>();
 
 watch(search, ([newSearch, oldSearch]) => newSearch !== oldSearch && (page.value = 0));
 
+const filterCount = ref(0);
 const extensions = ref([]);
+
+const pageCount = computed(() => Math.round(filterCount.value / perPage));
 
 watchEffect(async () => {
 	const { data } = await api.get('/extensions/registry', {
 		params: {
 			text: search.value,
-			limit: perPage.value,
-			offset: page.value * perPage.value,
+			limit: perPage,
+			offset: (page.value - 1) * perPage,
 			type: type.value,
 		},
 	});
 
+	filterCount.value = data.meta.filter_count;
 	extensions.value = data.data;
 });
 </script>
@@ -63,11 +68,22 @@ watchEffect(async () => {
 		</template>
 
 		<div class="page-container">
-			<ul>
-				<li v-for="extension in extensions" :key="extension.name">
-					{{ extension.name }}
-				</li>
-			</ul>
+			<button @click="type = undefined">All</button>
+			<button v-for="extType in EXTENSION_TYPES" :key="extType" @click="type = extType">{{ extType }}</button>
+
+			<v-list>
+				<v-list-item v-for="extension in extensions" :key="extension.name" block clickable>
+					<v-list-item-icon>
+						<div class="icon"><v-icon name="storefront" /></div>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<p>{{ extension.name }}</p>
+						<p>{{ extension.description }}</p>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+
+			<v-pagination v-if="pageCount > 1" v-model="page" :length="pageCount" :total-visible="5" />
 		</div>
 	</private-view>
 </template>
