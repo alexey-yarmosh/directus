44ccdb1e20cda814acf18ec0904d66f2ccb2e561
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-10-07 12:10:51 +0200

    feat: update default role id

--- init.sh
+++ init.sh
@@ -13,3 +13,9 @@ rm -rf api/lib
 
 echo '3. Copying extensions'
 tsx sync-extensions.ts
+
+echo '4. Updating AUTH_GITHUB_DEFAULT_ROLE_ID'
+cd '/Users/baderfall/Documents/web/jsd/globalping-dash-directus'
+GITHUB_ROLE_ID=$(grep '^AUTH_GITHUB_DEFAULT_ROLE_ID=' .env.development | cut -d'=' -f2)
+cd '/Users/baderfall/Documents/web/jsd/directus-repo-2'
+sed -i '' "s/^AUTH_GITHUB_DEFAULT_ROLE_ID=.*/AUTH_GITHUB_DEFAULT_ROLE_ID=$GITHUB_ROLE_ID/" api/.env
